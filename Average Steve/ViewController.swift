2040d37867e8698afca009abc8bb57093d811d1a
jasonhilimire <jasonhilimire@mac.com>
2018-03-05 20:17:20 -0700

    Initial Commit

---
+++ Average Steve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Average Steve
+//
+//  Created by Jason Hilimire on 3/5/18.
+//  Copyright © 2018 Peanut Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
